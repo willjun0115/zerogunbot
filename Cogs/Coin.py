8355c895f83200f861dae844cfa65dad554c80b7
DaiDail <willjun0115@gmail.com>
2021-05-11 21:42:26 +0900

    0

--- Cogs/Coin.py
+++ Cogs/Coin.py
@@ -327,8 +327,6 @@ class Coin(commands.Cog, name="코인(Coin)"):
             for role in ctx.guild.roles:
                 if role.position == int(num) + 3:
                     arole = role
-            if arole in ctx.author.roles:
-                await ctx.send("이미 " + arole.name + "을(를) 보유하고 있습니다.")
             else:
                 if int(num) == 1:
                     role_price = 50

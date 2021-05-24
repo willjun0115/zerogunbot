2a827427a691b9ad4ae334b9faf9e83ad4f9198d
DaiDail <willjun0115@gmail.com>
2021-05-24 11:14:55 +0900

    0

--- Cogs/Coin.py
+++ Cogs/Coin.py
@@ -250,8 +250,6 @@ class Coin(commands.Cog, name="코인(Coin)"):
         embed = discord.Embed(title="<코인 상점>",
                               description="%구매 (역할 번호) 로 구매해주세요\n"
                                           "'매니저' 역할 보유 시, 50% 할인!")
-        for role in ctx.guild.roles:
-            embed.add_field(name='> '+str(role.position-2)+'. '+role.name, value=':coin: ', inline=True)
         embed.add_field(name='> 13. 매니저', value=':coin: 100000', inline=True)
         embed.add_field(name='> 12. 스틸', value=':coin: 40000', inline=True)
         embed.add_field(name='> 11. 가챠 확장팩', value=':coin: 25000', inline=True)

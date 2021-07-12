62fc2af85c6c6de37f629d38faca5167a8e795a3
DaiDail <willjun0115@gmail.com>
2021-07-13 07:59:35 +0900

    0

--- Cogs/Voice.py
+++ Cogs/Voice.py
@@ -23,6 +23,12 @@ class Voice(commands.Cog, name="음성(Voice)"):
         channel = ctx.message.author.voice.channel
         await channel.connect()
 
+    @commands.command(name="잠수", help="", usage="%잠수")
+    async def submerge(self, ctx):
+        channel = ctx.guild.get_channel(760198518987685949)
+        await ctx.message.author.move_to(channel)
+        await ctx.send(ctx.message.author.name + " 님을 잠수방으로 옮겼습니다.")
+
 
 def setup(app):
     app.add_cog(Voice(app))

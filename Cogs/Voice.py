2374c1940158824a33103c8d468acb563d162a6c
DaiDail <willjun0115@gmail.com>
2021-09-16 18:13:02 +0900

    0

--- Cogs/Voice.py
+++ Cogs/Voice.py
@@ -15,11 +15,28 @@ class Voice(commands.Cog, name="음성(Voice)"):
     def __init__(self, app):
         self.app = app
 
+    async def find_queue(self, ctx):
+        queue_channel = ctx.guild.get_channel(887984694866632724)
+        queue_list = []
+        async for message in queue_channel.history(limit=30, oldest_first=True):
+            if message.content.startswith('https://www.youtube.com/') is True:
+                queue_list.append(message)
+            else:
+                await message.delete()
+        return queue_list
+
+    async def play_next(self, ctx, voice_client: discord.VoiceClient):
+        voice_client.stop()
+        queue = await self.find_queue(ctx)
+        await queue[0].delete()
+        if len(queue) > 0:
+            await self.play_song(ctx, queue[0].content)
+
     @commands.command(
         name="연결", aliases=["connect", "join"],
         help="음성 채널에 연결합니다.", usage="%*"
     )
-    async def join(self, ctx):
+    async def join_ch(self, ctx):
         if get(ctx.guild.roles, name='DJ') in ctx.message.author.roles:
             channel = ctx.message.author.voice.channel
             voice = get(self.app.voice_clients, guild=ctx.guild)
@@ -35,7 +52,7 @@ class Voice(commands.Cog, name="음성(Voice)"):
         name="퇴장", aliases=["연결해제", "연결끊기", "disconnect", "dc", "leave"],
         help="음성 채널을 나갑니다.", usage="%*"
     )
-    async def leave(self, ctx):
+    async def leave_ch(self, ctx):
         if get(ctx.guild.roles, name='DJ') in ctx.message.author.roles:
             await ctx.guild.voice_client.disconnect()
             await ctx.send("연결을 끊습니다.")
@@ -55,15 +72,16 @@ class Voice(commands.Cog, name="음성(Voice)"):
         name="재생", aliases=["play", "p"],
         help="유튜브 url을 통해 음악을 재생합니다.", usage="%* str(url)", pass_context=True
     )
-    async def play(self, ctx, url: str):
+    async def play_song(self, ctx, url: str):
         if get(ctx.guild.roles, name='DJ') in ctx.message.author.roles:
+            queue_channel = ctx.guild.get_channel(887984694866632724)
             voice = get(self.app.voice_clients, guild=ctx.guild)
             if voice and voice.is_connected():
                 try:
                     if os.path.isfile("0.mp3"):
                         os.remove("0.mp3")
                 except PermissionError:
-                    await ctx.send("현재 음악이 재생 중입니다. 끝날때까지 기다리시거나, 음악을 중지해주세요.")
+                    await queue_channel.send(url)
                 else:
                     msg = await ctx.send("재생 준비 중...")
                     ydl_opt = {
@@ -82,7 +100,7 @@ class Voice(commands.Cog, name="음성(Voice)"):
                             os.rename(file, "0.mp3")
                     await msg.edit(content="재생 시작")
                     source = FFmpegPCMAudio(source="0.mp3")
-                    voice.play(source, after=lambda e: voice.stop())
+                    voice.play(source, after=lambda e: self.play_next(ctx, voice))
         else:
             await ctx.send(" :no_entry: 이 명령을 실행하실 권한이 없습니다.")
 
@@ -90,7 +108,7 @@ class Voice(commands.Cog, name="음성(Voice)"):
         name="정지", aliases=["stop", "s"],
         help="음악 재생을 정지합니다.", usage="%*"
     )
-    async def stop(self, ctx):
+    async def stop_song(self, ctx):
         if get(ctx.guild.roles, name='DJ') in ctx.message.author.roles:
             voice = get(self.app.voice_clients, guild=ctx.guild)
             if voice and voice.is_connected():

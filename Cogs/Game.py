9f5f136744b4df8e46573f8e16d72e6c64e5697d
DaiDail <willjun0115@gmail.com>
2021-09-23 04:55:53 +0900

    0

--- Cogs/Game.py
+++ Cogs/Game.py
@@ -261,8 +261,7 @@ class Game(commands.Cog, name="게임", description="오락 및 도박과 관련
             elif lv == get(ctx.guild.roles, name="언랭").position:
                 win = 0
             if rand <= win * 0.01:
-                await member.remove_roles(member, member.top_role,
-                                          reason=f"{ctx.author.name} 님의 신고로 {member.name} 님이 제재받았습니다.")
+                await member.remove_roles(member.top_role)
                 embed.add_field(name="신고 접수", value="감사합니다. 신고가 접수되었습니다.\n" + member.name + " 님이 강등됩니다.",
                                 inline=True)
             else:

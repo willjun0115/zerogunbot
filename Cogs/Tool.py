4211be939550807e12179ebe54bd7c2e14ca7d4c
DaiDail <willjun0115@gmail.com>
2021-09-23 23:38:15 +0900

    0

--- Cogs/Tool.py
+++ Cogs/Tool.py
@@ -102,7 +102,7 @@ class Tool(commands.Cog, name="도구", description="정보 조회 및 편집에
                                   description=f"\"{args}\"의 검색 결과 :mag:")
             pre_path = '//div[@class="gallery-content"]/div'
             for n in range(0, 5):
-                get_title = browser.find_elements_by_xpath(pre_path + f'[{n}]/h1/a[0]').text
+                get_title = browser.find_elements_by_xpath(pre_path + f'[{str(n)}]/h1/a[0]').text
                 #get_artist = browser.find_elements_by_xpath(pre_path + f'[{n}]/div[@class="artist-list"]/ul/li/a[0]').text
                 embed.add_field(name=f"> {str(n + 1)}. " + get_title, value="none", inline=False)
             await ctx.send(embed=embed)

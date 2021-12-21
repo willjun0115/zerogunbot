04c879c99553f230e1d63433bb6d87ce86a47027
willjun0115 <willjun0115@gmail.com>
2021-12-21 19:58:49 +0900

    버그 수정

--- Cogs/Tool.py
+++ Cogs/Tool.py
@@ -178,10 +178,10 @@ class Tool(commands.Cog, name="도구", description="정보 조회 및 편집에
             if log is not None:
                 if val[0] == '+':
                     val = val[1:]
-                    await log.edit(content=log.content[:20] + str(int(log.content[:20]) + int(val)))
+                    await log.edit(content=log.content[:20] + str(int(log.content[20:]) + int(val)))
                 elif val[0] == '-':
                     val = val[1:]
-                    await log.edit(content=log.content[:20] + str(int(log.content[:20]) - int(val)))
+                    await log.edit(content=log.content[:20] + str(int(log.content[20:]) - int(val)))
                 else:
                     await log.edit(content=log.content[:20] + str(val))
                 await ctx.send('로그를 업데이트했습니다.')

106571e58a9dede7b9af835a0b872197f8b8889f
willjun0115 <willjun0115@gmail.com>
2022-01-21 07:04:42 +0900

    bot status command fixed

--- Main.py
+++ Main.py
@@ -106,8 +106,7 @@ async def bot_status_(ctx):
         name="> Client",
         value=f"client_name : {app.user.name}\n"
               f"client_id : {app.user.id}\n"
-              f"created_at : {app.user.created_at}\n"
-              f"locale : {app.user.locale}",
+              f"created_at : {app.user.created_at}",
         inline=False
     )
     await ctx.send(embed=embed)

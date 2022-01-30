ce54238c2f0fc40f5ccf82785b51f98e80cd6c16
willjun0115 <willjun0115@gmail.com>
2022-01-31 03:50:37 +0900

    admin task update init.

--- Cogs/Tool.py
+++ Cogs/Tool.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 from discord.utils import get
 import asyncio
 from selenium import webdriver
@@ -7,6 +7,79 @@ from selenium.webdriver.chrome.options import Options
 import os
 import youtube_dl
 from discord import FFmpegPCMAudio
+import time
+import pyupbit
+import datetime
+import numpy as np
+
+
+access = os.environ.get("ACCESS")
+secret = os.environ.get("SECRET")
+
+
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+
+def get_ror(k=0.5):
+    df = pyupbit.get_ohlcv("KRW-BTC", count=7)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+
+def find_best_k():
+    best_k = 0.5
+    best_ror = 0
+    for k in np.arange(0.1, 1.0, 0.1):
+        ror = get_ror(k)
+        if best_ror < ror:
+            best_ror = ror
+            best_k = k
+    return best_k
+
+
+upbit = pyupbit.Upbit(access, secret)
 
 
 class Tool(commands.Cog, name="도구", description="다양한 기능의 명령어 카테고리입니다."):
@@ -31,6 +104,38 @@ class Tool(commands.Cog, name="도구", description="다양한 기능의 명령�
             cc = chr(x)
             args = args + cc
         return str(args)
+
+    @tasks.loop(seconds=1.0)
+    async def BTCAutoTrade(self):
+        try:
+            now = datetime.datetime.now()
+            start_time = get_start_time("KRW-BTC")
+            end_time = start_time + datetime.timedelta(days=1)
+
+            if start_time < now < end_time - datetime.timedelta(seconds=30):
+                k = find_best_k()
+                target_price = get_target_price("KRW-BTC", k)
+                current_price = get_current_price("KRW-BTC")
+                if target_price < current_price:
+                    krw = get_balance("KRW")
+                    if krw > 5000:
+                        upbit.buy_market_order("KRW-BTC", krw * 0.9995)
+                        print(f"buy {krw * 0.9995} KRW-BTC")
+            else:
+                btc = get_balance("BTC")
+                if btc > 0.00008:
+                    upbit.sell_market_order("KRW-BTC", btc * 0.9995)
+                    print(f"sell {btc * 0.9995} KRW-BTC")
+        except Exception as e:
+            print(e)
+
+    @commands.command(
+        name="BTC", aliases=["자동매매"],
+        help="비트코인 자동매매 태스크 실행 확인 명령어", usage="*", hidden=True
+    )
+    async def BTCAutoTrade_check(self, ctx):
+        if self.BTCAutoTrade.is_running():
+            await ctx.send("BTC AutoTrade is running")
 
     @commands.command(
         name="도움말", aliases=["help", "?"],
